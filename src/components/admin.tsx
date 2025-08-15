3ead0b5f70b215c7e55ba06ba6ffdaa90608d54b
bhaskar <bassnaidu1242@gmail.com>
2025-08-15 13:13:02 +0530

    final check done

--- src/components/admin.tsx
+++ src/components/admin.tsx
@@ -23,7 +23,7 @@ const AdminContacts: React.FC = () => {
 
   useEffect(() => {
     if (isAuthenticated) {
-      fetch('https://microsoft-planner-for-project-manag-three.vercel.app/api/contact')
+      fetch('https://microsoft-planner-for-project-management.onrender.com/api/contact')
         .then((res) => res.json())
         .then((data) => setContacts(data))
         .catch((err) => console.error('Error fetching contacts:', err));
